b6427247722ad3c408b3bba09eeff0eac6a1df48
shangeet sankar <shangeetsankar@shangeets-MacBook-Pro.local>
2026-03-06 14:55:37 +0300

    Step 2: Add document loader function

--- gemini_qa_bot.py
+++ gemini_qa_bot.py
@@ -5,9 +5,18 @@ import gradio as gr
 # Load environment variables (like your Gemini API Key)
 load_dotenv()
 
+from langchain_community.document_loaders import PyPDFLoader
+
 # We will build our functions here step by step
 # ----------------------------------------------
 
+# Step 2: Document Loader
+# This function takes the uploaded PDF file from Gradio and loads its text.
+def document_loader(file_path):
+    # PyPDFLoader helps us read the text inside a PDF Document
+    loader = PyPDFLoader(file_path)
+    loaded_document = loader.load()
+    return loaded_document
 
 # ----------------------------------------------
 # Create the Gradio interface
